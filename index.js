568c201d6929b4d452308a586a4f5bb866b27cc5
Александр Демиденко <mr.alexdemidenko@gmail.com>
2021-07-04 19:04:09 +0400

    wip: recursive handling files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const minify = require('html-minifier-terser').minify;
+const minify = require("html-minifier-terser").minify;
 const STParser = require("simple-text-parser");
 
 const Parser = STParser.Parser;
@@ -10,104 +10,107 @@ class PhpHtmlPlugin {
 
     constructor(options) {
         this.userOptions = options || {};
+        const defaultOptions = {
+            include: "./php",
+            exclude: [],
+            minify: process.env.NODE_ENV === "production",
+            assetsFileName: "assetsMap.php",
+            minifierOptions: {
+                continueOnParseError: true,
+                collapseWhitespace: true,
+                removeComments: true,
+            },
+            recursive: true,
+            insertion: undefined,
+        };
+
+        this.options = Object.assign(defaultOptions, options || {});
+        const minifierOptions = this.options.minifierOptions;
+        if (minifierOptions.ignoreCustomFragments) {
+             minifierOptions.ignoreCustomFragments = [...minifierOptions.ignoreCustomFragments, phpTag];
+        } else {
+            minifierOptions.ignoreCustomFragments = [phpTag];
+        }
     }
 
-    // todo: зависимости для хот релоад
+    // todo: hot reload
     apply(compiler) {
         compiler.hooks.done.tap("PhpHtmlPlugin", (stats) => {
-
-            const defaultOptions = {
-                include: "./php",
-                exclude: [],
-                minify: process.env.NODE_ENV === "production",
-                assetsFileName: "assetsMap.php",
-                minifierOptions: {
-                    continueOnParseError: true,
-                    collapseWhitespace: true,
-                    removeComments: true,
-                },
-                deepHandle: true,
-                insertion: true,
-            };
-
-            this.options = Object.assign(defaultOptions, this.userOptions);
-            const minifierOptions = this.options.minifierOptions;
-            if (minifierOptions.ignoreCustomFragments) {
-                minifierOptions.ignoreCustomFragments = [...minifierOptions.ignoreCustomFragments, phpTag];
-            } else {
-                minifierOptions.ignoreCustomFragments = [phpTag];
-            }
-
-
             const target = path.resolve(stats.compilation.compiler.context, this.options.include);
 
             if (fs.statSync(target).isDirectory()) {
-                this.handleDir(stats, target);
+                this.handleDir(stats.compilation.compiler.context.outputPath, target);
             } else {
-                this.handleFile(stats, target);
+                this.handleFile(stats.compilation.compiler.context.outputPath, target);
             }
         })
     }
 
-    handleDir(stats, target) {
-        fs.readdir(target, function(err, files) {
-            files.forEach(function(fileName) {
-                //todo: deep exclude
-                if (this.options.exclude.includes(fileName)) { 
-                    return
-                }
-                if (fs.statSync(`${target}/${fileName}`).isDirectory() ) {
-                    if (!this.options.deepHandle) {
+    handleDir(outputPath, target, targetDir = "") {
+        fs.readdir(target, (err, files) => {
+            if (err) {
+                throw err;
+            }
+            files.forEach((name) => {
+                if (fs.statSync(path.resolve(target, name).isDirectory() )) {
+                    if (!this.options.recursive) {
                         return
                     }
-                    // todo: create dir
-                    this.handleDir(stats, `${target}/${fileName}`);
+                    fs.mkdir(path.resolve(outputPath, name));
+                    this.handleDir(outputPath, path.resolve(target, name), path.join(targetDir, name));
                 } else {
-                    this.handleFile(stats, fileName);
+                    this.handleFile(outputPath, path.resolve(target, name), name, targetDir);
                 }
                 
-            })
-        })
+            });
+        });
     }
 
-    handleFile(stats, fileName) {
+    handleFile(outputPath, filePath, fileName, targetDir) {
         if (fileName.match(/\.php$/i)) {
-            let fileData = fs.readFileSync(fileName).toString();
+            let fileData = fs.readFileSync(filePath).toString();
 
             const parser = new Parser();
-            parser.addRule(/<\?php([\s\S]+?)\?>/gi, function (tag, clean_tag) {
+            parser.addRule(/<\?php([\s\S]+?)\?>/gi, function (tag, cleanTag) {
                 return { type: "php", text: tag };
             });
             let ast = parser.toTree(fileData);
             if (this.options.minify) {
-                //парсинг файла для возврата пхп тэгов
-                //HTMLMinifier can't work with invalid or partial chunks of markup.
-                const minifyAst = parser.toTree(minify(fileData, this.options.minifierOptions));
-                const phpTags = ast.filter(item => item.type === "php");
-                minifyAst.forEach(item => {
-                    if (item.type === "php") {
-                        item.text = phpTags.shift().text;
-                    }
-                });
-                ast = minifyAst;
+                this.htmlMinify(ast, fileData);
             }
 
             ast.unshift({ tag: "php", text: `<?php require_once "${this.options.assetsFileName}" ?>` });
             if (this.options.insertion) {
                 if (Array.isArray(this.options.insertion)) {
-                    // todo: deep
                     if (this.options.insertion.includes(fileName)) {
                         ast.unshift({ tag: "php", text: this.insertion() });
                     }
                 } else {
-
+                    throw new Error("Option insertion must be array");
                 }
+            } else {
+                ast.unshift({ tag: "php", text: this.insertion() });
             }
 
             fileData = Parser.renderTree(ast);
             //todo: path
-            fs.writeFileSync(`${stats.compilation.compiler.outputPath}/index.php`, fileData);
+            fs.writeFileSync(path.resolve(outputPath, targetDir, fileName), fileData);
         }
+    }
+
+    htmlMinify(ast, fileData) {
+        // parse HTML without php tags
+        // HTMLMinifier can't work with invalid or partial chunks of markup.
+        const minifyAst = parser.toTree(
+            minify(fileData, this.options.minifierOptions)
+        );
+        const phpTags = ast.filter((item) => item.type === "php");
+        minifyAst.forEach((item) => {
+            if (item.type === "php") {
+                item.text = phpTags.shift().text;
+            }
+        });
+        ast = minifyAst;
     }
 
     insertion() {
